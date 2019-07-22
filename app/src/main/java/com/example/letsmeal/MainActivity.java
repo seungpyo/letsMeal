e082154ba86eff06bf716d969b60da4e4c514374
2017030500 <2017030500@hanyang.ac.kr>
2019-07-22 11:24:13 +0900

    Implemented drawer view, and moved logout FAB in drawer.

--- app/src/main/java/com/example/letsmeal/MainActivity.java
+++ app/src/main/java/com/example/letsmeal/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.letsmeal;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.AsyncTask; // We should use AsyncTask later...!
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -19,9 +26,9 @@ import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -29,11 +36,15 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+    implements NavigationView.OnNavigationItemSelectedListener {
 
 
     private final String TAG = "MainActivity";
     private final int CREATE_SCHEDULE_REQ = 100;
+
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavView;
 
     RecyclerView recyclerView;
     RecyclerViewAdapter recyclerViewAdapter;
@@ -56,8 +67,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar mainToolbar = findViewById(R.id.mainToolBar);
+        setSupportActionBar(mainToolbar);
+        //추가된 소스코드, Toolbar의 왼쪽에 버튼을 추가하고 버튼의 아이콘을 바꾼다.
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_dehaze_black_24dp);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mDrawerLayout.addDrawerListener(new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                mainToolbar,
+                R.string.open,
+                R.string.close
+        ));
+        mNavView = findViewById(R.id.nav_view);
+        mNavView.setNavigationItemSelectedListener(this);
+
+
         /**
          * Initialize RecyclerView
+         *
+         *
          */
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView = findViewById(R.id.recyclerview);
@@ -95,32 +126,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        /*
         final FloatingActionButton logOutFab = findViewById(R.id.logOutFab);
         logOutFab.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
-                AuthUI.getInstance()
-                        .signOut(MainActivity.this)
-                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                            public void onComplete(@NonNull Task<Void> task) {
-                                // ...
-                            }
-                        });
-
-                SharedPreferences.Editor editor =
-                        getSharedPreferences("pref", MODE_PRIVATE).edit();
-                editor.putBoolean("signInRequired", true);
-                editor.putString("uid", null);
-                editor.commit();
-                startActivity(new Intent(MainActivity.this, SplashActivity.class));
-                finish();
+                logout();
             }
         });
+        */
 
 
+
+    }
+
+    private void logout() {
+        AuthUI.getInstance()
+                .signOut(MainActivity.this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
+                    }
+                });
+
+        SharedPreferences.Editor editor =
+                getSharedPreferences("pref", MODE_PRIVATE).edit();
+        editor.putBoolean("signInRequired", true);
+        editor.putString("uid", null);
+        editor.commit();
+        startActivity(new Intent(MainActivity.this, SplashActivity.class));
+        finish();
     }
 
     @Override
@@ -154,6 +191,41 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_logout:
+                logout();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        switch(id) {
+            case R.id.action_logout:
+                logout();
+                return true;
+        }
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
     /**
      * An internal class fetching initial schedules from FireStore.
